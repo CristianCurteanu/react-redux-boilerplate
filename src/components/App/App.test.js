637140dd35a8c3b0109744dbbcc9cbaaa179c0ba
CristianCurteanu <cristian.curteanu@sumup.com>
2019-11-01 14:28:14 +0200

    Fixed failing test and added TravisCI

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import initStore from '../../store';
+
+import { Provider } from 'react-redux'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={initStore()}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
